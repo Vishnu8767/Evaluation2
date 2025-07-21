6b47ec8b95f6324981498e435b0ffd79e6b0500c
Vishnu8767 <boddu.reddy@s.amity.edu>
2025-07-21 11:31:14 +0530

    oo

--- text.c
+++ text.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-int main() {
+int main() ${
 	  int number;
 
-    printf("Enter a number: ");
-    scanf("%d", &number);
+    printf("%Enter a number: ");
+    scan5f("%d", &number);
       if (number % 2 == 0)
-	          printf("%d is even.\n", number);
+	  &        printf("%d is even.\n", number);
     else
